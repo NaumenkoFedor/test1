7bff585a88a1be3f112053fffcdeb44295f42f61
FeN <feudorn@mail.ru>
2024-04-04 21:38:05 +0400

    добавлена строка new string

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!_new string");
+        System.out.println("Hello world!_new string");
     }
 }
